39523868a75d96774f084490e7ec52fa6927a714
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2026-06-30 13:56:10 +0530

    fix(lib): conditionally apply flex-col default in InvoiceSection

--- packages/react-invoice/src/components/section.tsx
+++ packages/react-invoice/src/components/section.tsx
@@ -2,12 +2,15 @@ import { forwardRef } from "react";
 
 export type InvoiceSectionProps = React.HTMLAttributes<HTMLDivElement>;
 
+const FLEX_DIRECTION_RE = /\bflex-(row|col|row-reverse|col-reverse)\b/;
+
 export const InvoiceSection = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   function InvoiceSection({ children, className, style, ...props }, ref) {
+    const hasDirection = className && FLEX_DIRECTION_RE.test(className);
     return (
       <div
         ref={ref}
-        className={`flex flex-col${className ? ` ${className}` : ""}`}
+        className={`flex${hasDirection ? "" : " flex-col"}${className ? ` ${className}` : ""}`}
         style={style}
         {...props}
       >
